6ccfee77bc5353a87baf127666ab1fa3d7773286
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2024-03-05 22:16:30 -0300

    Iniciando

---
+++ src/app/index/styles.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native'
+import {theme} from "@/theme"
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24
+  },
+  title: {
+
+  },
+  subtitle: {
+
+
+  }
+})
